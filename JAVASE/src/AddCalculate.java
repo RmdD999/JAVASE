4dc5f7402c2b670e9b339243a7036149ce8ccd37
RmdD99 <105860734+RmdD999@users.noreply.github.com>
2022-12-05 14:58:55 +0800

    20221205
    as title

--- JAVASE/src/AddCalculate.java
+++ JAVASE/src/AddCalculate.java
@@ -10,8 +10,14 @@ public class AddCalculate {
 //        System.out.println("result is :" + result);
 //        swapTwoNums();
 //        swapTwoNums2();
-        swapTwoNums3();
+//        swapTwoNums3();
+//        switchCase();
+//        forLoop();
+//        GuessNums.guessNum();
+//        primeNumber();
+        fibonacci();
     }
+
 
     //the place that memeory assigned to 'a' could be different
     public void Calculation2(){
@@ -90,5 +96,66 @@ public class AddCalculate {
         System.out.println(a);
         System.out.println(b);
 
+    }
+
+    //switch case
+    //default 的位置与结果无关， 缺少break的语句会发生case穿透--忽视下方的case判定的存在， 直接执行case后面的代码块
+    public static void switchCase(){
+
+        int a =10, b = 20;
+        System.out.println(a>b?a:b);
+
+        switch(a){
+            default :System.out.println("default");break;
+            case 10 : System.out.println("10");
+            case 5 : System.out.println("5");
+            case 6 : System.out.println("6"); break;
+            case 7 : System.out.println("7"); break;
+            case 20 : System.out.println("20"); break;
+        }
+    }
+
+    //for loop
+    // advice on using for loop: 1. recorder start from 0 or 1. 2.
+    public static void forLoop(){
+        for(int i =0; i < 7; i++) {
+            System.out.println("Hello World.");
+        }
+    }
+
+    //find out all prime numbers from 1-100
+    public static void primeNumber() {
+
+        for(int i = 1; i < 100; i++) {
+
+            int j = 2;
+            while(j <= i){
+                if(i%j == 0){
+                    break;
+                }
+                j++;
+            }
+
+            if(j==i) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    //fibonacci sequence
+    public static void fibonacci(){
+
+        long sum = 0;
+        long front = 0;
+        long back = 1;
+
+        for(int k = 1; k < 50; k++) {
+            if(k==1)
+                System.out.println(k);
+            sum = front + back;
+            System.out.println(sum);
+            front = back;
+            back = sum;
+        }
     }
 }
