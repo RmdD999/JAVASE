cb1922f0a201a6a937dc6e7557c2ef95f2eb3b04
RmdD99 <105860734+RmdD999@users.noreply.github.com>
2022-12-16 15:25:44 +0800

    Generic 20221216

---
+++ JAVASE/src/GenericDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class GenericDemo {
+//    List<String> stringArrList = new ArrayList<String>();
+//    List<Integer> intArrList = new ArrayList<Integer>();
+//
+//    Class classStringArrayList = stringArrList.getClass();
+//    Class classIntegerArrayList = intArrList.getClass();
+
+//    if(classStringArrayList.equals(classIntegerArrayList)){
+//        Log.d("泛型测试","类型相同");
+//    }
+
+    //泛型类型在逻辑上可以看成多种类型，但是实际上是同一种类型
+
+}
+
+
---
+++ JAVASE/src/GenericTest.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class GenericTest {
+    //T创建对象的时候指定具体的类型
+    public class Generic<T>{
+        private T key;
+
+        public Generic(T key){
+            this.key = key;
+        }
+
+
+        public T getKey(){
+            return key;
+        }
+
+        //泛型方法public 和<T>不可少
+        public <T> T showKeyName(Generic<T> container){
+            System.out.println("container key: "+container.getKey());
+            T test = container.getKey();
+            return test;
+        }
+
+        public static void main(String[] args){
+            //编译检查类型
+            //减少数据类型转换
+            ArrayList<String> strList = new ArrayList<>();
+            strList.add("a");
+            strList.add("b");
+            strList.add("c");
+
+        }
+    }
+}
