8332ae244a4e9e163dee127e2d6d4662d5f9899d
RmdD99 <105860734+RmdD999@users.noreply.github.com>
2022-12-29 13:01:53 +0800

    IO

---
+++ JAVASE/src/ExceptionDemo/ExceptionDemo.java
@@ -0,0 +1,10 @@
+package ExceptionDemo;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        //异常分类
+        //编译异常 -- 程序还没有开始运行时发生异常， java.lang.Exception
+        //运行异常 -- 运行过程中发生异常 java.lang.RuntimeException
+        //ERROR java.lang.Error 这种异常是最难解决的，可能源于环境问题，内存耗尽，杀毒软件blocker。
+    }
+}
---
+++ JAVASE/src/IODemo/IODemo.java
@@ -0,0 +1,17 @@
+package IODemo;
+
+public class IODemo {
+    public static void main(String[] args) {
+        //什么是IO流？
+        //存储和读取的解决方案 input output
+
+        //IO流的作用？
+        //读写数据
+
+        //IO流按照流向分可以分成什么？
+        //输出流--》文件 输入流--》读取
+
+        //IO流按照文件类型来分可以分成什么？
+        //字节流--》all type  字符流--》可以被记事本打开并不影响其内容的文件 txt md,xml lrc.
+    }
+}
