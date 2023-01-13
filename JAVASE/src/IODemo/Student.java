aa7d5582bf344c1c6ee00c5968e604ed4e3f2255
RmdD99 <105860734+RmdD999@users.noreply.github.com>
2023-01-13 20:58:19 +0800

    20230113

--- JAVASE/src/IODemo/Student.java
+++ JAVASE/src/IODemo/Student.java
@@ -3,8 +3,11 @@ package IODemo;
 import java.io.Serializable;
 
 public class Student implements Serializable {//标记性接口 一旦实现 表示当前类可以被序列化
+    private static final long serialVersionUID = 1L;
     private String name;
     private int age;
+
+    private transient String address; // 如果不想被序列化
 
     public Student(){
 
