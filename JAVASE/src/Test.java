b3a3c4545a93c01cc7bbe44c406f318b62a21a0a
RmdD99 <105860734+RmdD999@users.noreply.github.com>
2023-04-05 16:02:24 -0700

    another way of FizzBuzz

--- JAVASE/src/Test.java
+++ JAVASE/src/Test.java
@@ -1,10 +1,15 @@
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         String[] res = FizzBuzz(15);
         System.out.println(Arrays.toString(res));
+
+        List<String> res2= FizzBuzz2(15);
+        System.out.println(res2);
     }
 
     public static String[] FizzBuzz(int number){
@@ -24,5 +29,28 @@ public class Test {
             }
         }
         return result;
+    }
+
+    public static List<String> FizzBuzz2(int number){
+        List<String> res = new ArrayList<>();
+
+        for(int i = 1; i <= number; i++){
+            boolean dividedBy3 = i%3==0;
+            boolean dividedBy5 = i%5==0;
+
+            String currStr = "";
+            if (dividedBy3) {
+                currStr+="Fizz";
+            }
+            if (dividedBy5) {
+                currStr+="Buzz";
+            }
+            if(currStr.isEmpty()){
+                currStr = String.valueOf(i);
+            }
+
+            res.add(currStr);
+        }
+        return res;
     }
 }
